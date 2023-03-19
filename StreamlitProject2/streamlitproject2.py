67c541572bcf53a8ac65df95478b5cf648496483
Fassai <fasaifit@gmail.com>
2023-03-20 00:40:21 +0700

    20.3.23

---
+++ StreamlitProject2/streamlitproject2.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import plost 
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+# Markdown 
+st.title("Warrix Health Sale & Marketing Dashboard")
+st.markdown("By Fasai")
+
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
+st.sidebar.header('Dashboard `version 2`')
+
+st.sidebar.subheader('Donut chart parameter')
+donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+
+st.sidebar.subheader('Line chart parameters')
+plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+plot_height = st.sidebar.slider('Specify plot height', 200, 500, 500)
+
+st.sidebar.markdown('''
+---
+Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
+''')
+
+
+# Row A
+st.markdown('### Metrics')
+col1, col2, col3 = st.columns(3)
+col1.metric("Online Revenue", "700,000 THB", "12 %")
+col2.metric("Offline Revenue", "300,000 THB", "")
+col3.metric("ROAS", "4:1", "")
+
+# Row B
+seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+
+c1, c2 = st.columns((7,3))
+with c1:
+    st.markdown('### Line chart')
+    st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
+with c2:
+    st.markdown('### Donut chart')
+    plost.donut_chart(
+        data=stocks,
+        theta=donut_theta,
+        color='company',
+        legend='bottom', 
+        use_container_width=True)
+
+# Row C
